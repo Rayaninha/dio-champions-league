657fe42687b440d901cc52a0bc19a05b8ade4c24
Rayaninha <salesrayane11@gmail.com>
2024-07-12 15:52:08 -0300

    feat: initial server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log("hello world");
+import express, { json, Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "hello world",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
