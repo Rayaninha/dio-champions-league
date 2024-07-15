4f0a545f25fb6ebe02aa52cb72d2e60523e3060f
Rayaninha <salesrayane11@gmail.com>
2024-07-15 09:36:18 -0300

    feat: repositories

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,7 +1,8 @@
+import * as PlayersRepository from "../repositories/players-repository";
 import { noContent, ok } from "../utils/http.helper";
 
 export const getPlayerService = async () => {
-  const data = { player: "name" };
+  const data = await PlayersRepository.findAllPlayers();
 
   let response = null;
 
